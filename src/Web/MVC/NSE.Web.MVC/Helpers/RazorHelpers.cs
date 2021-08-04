8c020231569356cbd771171c90e724e43eab9e45
Augusto Pereira <augusto.pereira@mgmcorp.com.br>
2021-08-04 16:11:06 -0300

    Decimal razor helper

---
+++ src/Web/MVC/NSE.Web.MVC/Helpers/RazorHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Web.MVC.Helpers
+{
+    public static class RazorHelpers
+    {
+        public static string ToCurrency(this decimal value)
+        {
+            return value == 0 ? "Free" : value.ToString("C");
+        }
+    }
+}
